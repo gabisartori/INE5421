d46966453719e68534caab6f5b1ff0ccfd4dea40
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2024-04-20 01:54:54 -0300

    Minimização de automatos quase pronta

--- atividades/fda.py
+++ atividades/fda.py
@@ -1,4 +1,4 @@
-from typing import Dict, FrozenSet, Set
+from typing import Dict, FrozenSet, Set, List
 
 class FDA:
     def __init__(self, string: str="") -> None:
@@ -151,8 +151,92 @@ class FDA:
         deterministic.string = str(deterministic)
         return deterministic
 
+    def equivalent_states(self) -> Dict[FrozenSet[str], FrozenSet[str]]:
+        def are_equivalent(state: FrozenSet[str], other_state: FrozenSet[str], previous_classes: List[FrozenSet[FrozenSet[str]]]) -> bool:
+            '''Verifica se dois estados são n_equivalentes, ou seja, se para toda transição, o estado destino pertence à mesma classe de equivalência n-1.'''
+            # Para cada símbolo do alfabeto, verifica se o estado destino da transição pertence à mesma classe de equivalência n
+            for symbol in self.alphabet:
+                # Calcula o destino de cada estado
+                next_state = min(self.transitions[state][symbol])
+                other_next_state = min(self.transitions[other_state][symbol])
+
+                # Busca a qual classe de equivalencia n cada estado pertence
+                next_state_class = None
+                other_next_state_class = None
+                for previous_class in previous_classes:
+                    if next_state in previous_class:
+                        next_state_class = previous_class
+                    if other_next_state in previous_class:
+                        other_next_state_class = previous_class
+                
+                # Se para algum símbolo as classes dos destinos forem diferentes, então os estados não são n+1 equivalentes
+                if next_state_class != other_next_state_class:
+                    return False
+            # Se nenhum símbolo encontrar umas classe destino diferente, então os estados são n+1 equivalentes
+            return True
+
+        def belong(state: FrozenSet[str], equivalence_class: FrozenSet[FrozenSet[str]], previous_classes: List[FrozenSet[FrozenSet[str]]]) -> bool:
+            '''Verifica se um estado pertence a uma classe de equivalência.'''
+            # Compara o estado com um estado qualquer da classe de equivalência
+            for present_state in equivalence_class:
+                return are_equivalent(state, present_state, previous_classes)
+
+        current_equivalence: List[FrozenSet[FrozenSet[str]]] = [self.final_states, self.states.difference(self.final_states)]
+        next_equivalence: List[FrozenSet[FrozenSet[str]]] = []
+        while True:
+            for equivalence_class in current_equivalence:
+                temp_equivalence: List[FrozenSet[FrozenSet[str]]] = []
+                for state in equivalence_class:
+                    state_placed = False
+                    # Coloca o estado na classe que ele pertence
+                    for i, possible_equivalence_class in enumerate(temp_equivalence):
+                        if belong(state, possible_equivalence_class, current_equivalence):
+                            temp_equivalence[i] = temp_equivalence[i].union(frozenset((state,)))
+                            state_placed = True
+                            break
+                    # Se o estado não pertencer a nenhuma classe, cria-se uma nova classe para ele
+                    if not state_placed:
+                        temp_equivalence.append(frozenset((state,)))
+                for new_class in temp_equivalence:
+                    next_equivalence.append(new_class)
+            
+            # Se o cálculo de n+1 equivalência dos estados for igual à n equivalência, encerra a conta
+            if current_equivalence == next_equivalence:
+                break
+            # Se não, esquece a equivalência atual e passa a calcular a próxima da próxima, para comparar n+1 com n+2
+            else:
+                current_equivalence, next_equivalence = next_equivalence, []
+
+        equivalent = {}
+        for state in self.states:
+            find = None
+            for equivalence_class in current_equivalence:
+                if state in equivalence_class: find = sorted([self.state_to_string(x) for x in equivalence_class])[0]
+            equivalent[state] = find        
+        return equivalent
+    
     def minimal_equivalent(self) -> 'FDA':
-        pass
+        equivalent_states = self.equivalent_states()
+        minimal = FDA()
+
+        minimal.alphabet = self.alphabet.copy()
+        minimal.initial_state = frozenset([equivalent_states[self.initial_state]])
+        minimal.final_states = frozenset([equivalent_states[state] for state in self.final_states])
+
+        minimal.transitions = {}
+        for state in self.states:
+            for symbol in self.transitions[state]:
+                next_state = min(self.transitions[state][symbol])
+                if equivalent_states[state] not in minimal.transitions:
+                    minimal.transitions[equivalent_states[state]] = {}
+                if symbol not in minimal.transitions[equivalent_states[state]]:
+                    minimal.transitions[equivalent_states[state]][symbol] = frozenset()
+                minimal.transitions[equivalent_states[state]][symbol] = minimal.transitions[equivalent_states[state]][symbol].union(frozenset([equivalent_states[next_state]]))
+        
+        minimal.states = frozenset(equivalent_states.values())
+        minimal.num_states = len(minimal.states)
+        minimal.string = str(minimal)
+        return minimal
 
     def copy(self) -> 'FDA':
         copy = FDA()
