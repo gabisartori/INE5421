91f0632fc49b0017a2f42dbfc9a81ab331f38d45
Gabriel Sartori Rangel <gabriel.sartorirangel@gmail.com>
2024-06-15 16:46:05 -0300

    Change first_follow is_terminal check from lower to not upper

--- atividades/first_follow.py
+++ atividades/first_follow.py
@@ -2,8 +2,13 @@ from fda import CFG
 
 input_1 = "P = KVC; K = cK; K = &; V = vV; V = F; F = fPiF; F = &; C = bVCe; C = miC; C = &;"
 input_2 = "P = KL; P = bKLe; K = cK; K = TV; T = tT; T = &; V = vV; V = &; L = mL; L = &;"
+input_3 = "S = SoA; S = A; A = AaB; A = B; B = nB; B = (S); B = t; B = f;"
 
-inputs = [input_1, input_2]
+# S -> S or A | A
+# A -> A and B | B
+# B -> not B | (S) | true | false
+
+inputs = [input_1, input_2, input_3]
 
 for input in inputs:
     cfg = CFG(input.strip())
